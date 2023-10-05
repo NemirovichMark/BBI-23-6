cb966ed76f9103bd04c4cde4552774dfbb2e9a1d
KruglovKirill <m2302196@edu.misis.ru>
2023-10-05 23:16:29 +0300

    Test2

--- Program.cs
+++ Program.cs
@@ -10,11 +10,7 @@ namespace ConsoleApp1
             double s = 0;
             int p = Int32.Parse(Console.ReadLine());
             int h = Int32.Parse(Console.ReadLine());
-            for (int i = 0; i <= 9; i++)
-            {
-                s += Math.Pow((p + h * i), 2);
-            }
-            Console.WriteLine(s);
+           
             #endregion
         }
     }
