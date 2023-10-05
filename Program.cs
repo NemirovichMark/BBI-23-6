6dc65f32063994c37da178f6bd1823f97a677767
KruglovKirill <m2302196@edu.misis.ru>
2023-10-05 23:15:25 +0300

    testst

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region
+            double s = 0;
+            int p = Int32.Parse(Console.ReadLine());
+            int h = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i <= 9; i++)
+            {
+                s += Math.Pow((p + h * i), 2);
+            }
+            Console.WriteLine(s);
+            #endregion
         }
     }
 }
