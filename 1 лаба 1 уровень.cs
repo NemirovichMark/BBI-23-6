ccb103d3939772811116ad83b2d4c0c655bba7bf
ArdashirovaAmina <146060724+ArdashirovaAmina@users.noreply.github.com>
2023-10-10 10:59:22 +0500

    Add files via upload

---
+++ 1 лаба 1 уровень.cs
@@ -0,0 +1,211 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                #region // 1 уровень 1 задача
+                double s = 0;
+                for (int i = 2; i < 36; i = i + 3)
+                {
+                    s = s + i;
+                }
+                Console.WriteLine(s);
+                Console.WriteLine("Конец 1 задачи");
+                #endregion
+                
+
+                #region // 1 уровень 2 задача
+                double su = 0;
+                for (int i = 1; i < 11; i++)
+                {
+                    su = su + 1 / i;
+                }
+                Console.WriteLine(su);
+                Console.WriteLine("Конец 2 задачи");
+                #endregion
+
+
+                #region // 1 уровень 3 задача
+                double m = 3, sem = 0;
+                for (double i = 2; i < 113; i = i + 2)
+                {
+                    sem = sem + i / m;
+                    m = m + 2;
+                }
+                Console.WriteLine(sem);
+                Console.WriteLine("Конец 3 задачи");
+                #endregion
+
+                #region // 1 уровень 4 задача
+                double x = 2, del = 1, so = 0;
+                for (int i = 1; i < 9; i++)
+                {
+                    so = so + Math.Cos(x * i) / del;
+                    del = del * x;
+                }
+                Console.WriteLine(so);
+                Console.WriteLine("Конец 4 задачи");
+                #endregion
+
+
+                #region // 1 уровень 5 задача
+                int h = 5, p = 4;
+                double si = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    si = si + (p + i * h) * (p + i * h);
+                }
+                Console.WriteLine(si);
+                Console.WriteLine("Конец 5 задачи");
+                #endregion
+
+                #region // 1 уровень 6 задача
+
+                double y = 0;
+                for (double x1 = -4; x1 <= 4; x1 += 0.5)
+                {
+                    y = 0.5 * x1 * x1 - 7 * x1;
+                    Console.WriteLine("x={0:F3} | y={1:F3}", x1, y);
+                }
+                Console.WriteLine("Конец 6 задачи");
+                #endregion
+
+                #region // 1 уровень 7 задача
+                int sum = 1;
+                for (int i = 1; i < 7; i++)
+                {
+                    sum = sum * i;
+                }
+                Console.WriteLine(sum);
+                Console.WriteLine("Конец 7 задачи");
+                #endregion
+
+                #region // 1 уровень 8 задача
+                int f = 1;
+                double sy = 0;
+                for (int i = 1; i < 7; i++)
+                {
+                    f = f * i;
+                    sy = sy + f;
+                }
+                Console.WriteLine(sy);
+                Console.WriteLine("Конец 8 задачи");
+                #endregion
+
+                #region // 1 уровень 9 задача
+                y = 1; sem = 0;
+                double xy = -1, z = 5;
+                for (int i = 1; i < 7; i++)
+                {
+
+                    sem = sem + x * z / y;
+                    y = y * i;
+                    z = z * 5;
+                    xy = xy * -1;
+                }
+                Console.WriteLine(sem);
+                Console.WriteLine("Конец 9 задачи");
+                #endregion
+
+                #region // 1 уровень 10 задача
+                double sam = 1, t = 3;
+                for (int i = 0; i < 8;)
+                {
+                    sam = t * sam;
+                    i = i + 1;
+                }
+                Console.WriteLine(sam);
+                Console.WriteLine("Конец 10 задачи");
+                #endregion
+
+                #region // 1 уровень 11a задача
+                for (int i = 1; i < 7; i++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine("Конец 11а задачи");
+                #endregion
+
+                #region // 1 уровень 11б задача
+                t = 5;
+                for (int i = 0; i < 7;)
+                {
+                    i = i + 1;
+                    Console.Write(t);
+                }
+                Console.WriteLine("Конец 11б задачи");
+                #endregion
+
+                #region // 1 уровень 12 задача
+                double xex = 7;
+                for (double i = 0; i < 11; i++)
+                {
+                    sam = sam + 1 / xex;
+                    xex = xex * 7;
+                }
+                Console.WriteLine(sam);
+                Console.WriteLine("Конец 12 задачи");
+                #endregion
+
+                #region // 1 уровень 13 задача
+                double ya = 0;
+                for (double xe = -1.5; xe < 1.5; xe += 0.1)
+                {
+                    if (xe <= -1)
+                    {
+                        ya = 1;
+                    }
+                    else if (xe > 1)
+                    {
+                        ya = -1;
+                    }
+                    else
+                    {
+                        ya = -xe;
+                    }
+
+                    Console.WriteLine("x= {0:f3} ya={1:f3}", xe, ya);
+                }
+                Console.WriteLine("Конец 13 задачи");
+                #endregion
+
+                #region // 1 уровень 14 задача
+                int perv = 1, vtor = 1, som = 0;
+                Console.WriteLine(perv);
+                Console.WriteLine(vtor);
+                for (int i = 1; i < 7; i++)
+                {
+                    sum = perv + vtor;
+                    Console.WriteLine(som);
+                    perv = vtor;
+                    vtor = som;
+                }
+                Console.WriteLine("Конец 14 задачи");
+                #endregion
+
+                #region // 1 уровень 15 задача
+                int a = 1, b = 2, c = a + b, d = 1, e = 1, g = d + e;
+                for (int i = 1; i <= 2; i++)
+                {
+                    a = b;
+                    b = c;
+                    c = a + b;
+                    d = e;
+                    e = g;
+                    g = d + e;
+                }
+                Console.WriteLine(c * 1.0 / g);
+                Console.WriteLine("Конец 15 задачи");
+                #endregion
+            }
+
+
+
+        }
+    }
+
+}
---
+++ 1 лаба 2 уровень.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                #region // 2 уровень 1 задача
+                double s = 0, n = 1, x = 5;
+                const double e = 0.0001;
+                while ((Math.Cos(n * x) / n * n) >= e)
+                {
+                    s = s + Math.Cos(n * x) / n * n;
+                    n = n + 1;
+                }
+                Console.WriteLine(s);
+                Console.WriteLine("Конец 1 задачи");
+                #endregion
+
+                #region // 2 уровень 2 задача
+                int so = 1, ne = 1;
+                const int l = 30000;
+                while (so <= l)
+                {
+                    so = so * ne;
+                    ne = ne + 3;
+                }
+                Console.WriteLine("n={0:F3}", ne);
+                Console.WriteLine("Конец 2 задачи");
+                #endregion
+
+                #region // 2 уровень 3 задача
+                int si = 0, na = 0, m;
+                const int a = 2, h = 3, p = 41;
+                while (si <= p)
+                {
+                    m = a + na * h;
+                    si = si + m;
+                    na = na + 1;
+                }
+                Console.WriteLine("na={0:F3}", na);
+                Console.WriteLine("Конец 3 задачи");
+                #endregion
+
+                #region // 2 уровень 4 задача
+                double sum = 1, k = 1;
+                x = Convert.ToDouble(Console.ReadLine());
+                if (Math.Abs(x) >= 1)
+                    Console.WriteLine("x>=1");
+                else
+                {
+                    while (k > 0.0001)
+                    {
+                        sum = sum + k;
+                        k = x * x * k;
+                    }
+                    Console.WriteLine("sum = {0}", sum);
+                }               
+                Console.WriteLine("Конец 4 задачи");
+                #endregion
+
+                #region // 2 уровень 5 задача
+                double N, M, ch = 0, ost = 0, w = 0;
+                N = Convert.ToDouble(Console.ReadLine());
+                M = Convert.ToDouble(Console.ReadLine());
+                while (N >= M)
+                {
+                    N = N - M;
+                    w++;
+                }
+                    ch = w;
+                    ost = N;
+                Console.WriteLine("ch = " + ch + " ost = " + ost);
+                Console.WriteLine("Конец 5 задачи");
+                #endregion
+
+                #region // 2 уровень 6 задача
+                int time = 0, klet = 10;
+                while (klet<100000)
+                {
+                    klet = klet * 2;
+                    time=time+3;
+                }
+                Console.WriteLine(time);
+                Console.WriteLine("Конец 6 задачи");
+                #endregion
+
+                #region // 2 уровень 7a задача
+                double distance = 10;
+                for (int day = 2; day <= 7; day++) // увеличение началось со второго дня
+                {
+                    distance *= 1.1; 
+                    Console.WriteLine("В день {0} спортсмен пробежал {1}км", day, distance);
+                }
+                #endregion
+
+                #region // 2 уровень 7b задача
+                double ros = 10, ms = 0, dp = 1.1, ss = 0;
+                while (s <= 100)
+                {
+                    ss += ros;
+                    ros *= dp;
+                    ms++;
+                }
+                Console.WriteLine(ms);
+                Console.WriteLine("Конец 7b задачи");
+                #endregion
+
+                #region // 2 уровень 7c задача
+                double Mes = 0, Dop = 0, Ras = 10;
+                while (Dop < 20)
+                {
+                    Dop = Ras / 100 * 10;
+                    Ras = Ras + Dop;
+                    Mes = Mes + 1;
+                }
+                Console.WriteLine(Mes);
+                Console.WriteLine("Конец 7c задачи");
+                #endregion
+
+                #region // 2 уровень 8 задача
+                double den = 10000, mes = 0, percent = 1.08;
+                while (den < 20000)
+                {
+                    den *= percent;
+                    mes=mes+1;
+                }
+                Console.WriteLine(mes);
+                Console.WriteLine("Конец 8 задачи");
+                #endregion
+            }
+
+        }
+    }
+}
