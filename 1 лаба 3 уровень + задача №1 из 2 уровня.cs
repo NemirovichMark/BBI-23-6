d4b48b746a739e98c939fd0dd966e8a3aa047f21
ArdashirovaAmina <146060724+ArdashirovaAmina@users.noreply.github.com>
2023-10-16 13:44:52 +0500

    Add files via upload

---
+++ 1 лаба 3 уровень + задача №1 из 2 уровня.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                #region // 2 уровень 1 задача
+                double se = 0, n = 1, xe = 5;
+                while ((Math.Cos(n * xe) / n * n) >= 0.0001)
+                {
+                    se = se + Math.Cos(n * xe) / n * n;
+                    n = n + 1;
+                }
+                Console.WriteLine(se);
+                Console.WriteLine("Конец 1 задачи");
+                #endregion
+
+                #region // 3 уровень 2 задача
+                double a = 0.1;
+                double b = 0.8;
+                double h = 0.1;
+
+                for (double x = a; x <= b; x += h)
+                {
+                    double s = 0;
+                    double y = (x * Math.Sin(Math.PI / 4)) / (1 - 2 * x * Math.Cos(Math.PI / 4) + x * x);
+                    double xi = x;
+                    int i = 1;
+
+                    while (Math.Abs(xi) >= 0.0001)
+                    {
+                        s += xi * Math.Sin(i * Math.PI / 4);
+                        i++;
+                        xi *= x;
+                    }
+
+                    Console.WriteLine("x = {0:F2}, s = {1:F3}, y = {2:F3}", x, s, y);
+                }
+                #endregion
+            }
+        }
+    }
+}
---
+++ конт.работа Ардаширова №1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region // контрольная работа вариант 10 №1
+            int s = 0;
+            for (int i = 3; i <= 21; i = i + 2)
+            {
+                s = s + (i / i + 1);
+            }
+            Console.WriteLine(s);
+            Console.WriteLine("Конец 1 задачи");
+            #endregion
+
+            #region // контрольная работа вариант 10 №2
+            double sum = 0, x = 0.5, t = 0, m;
+            do
+            {
+                t = t + 2;
+                m = Math.Pow(x, t);
+                sum = sum + m;
+            }
+            while (m > 0.0001);
+            Console.WriteLine(sum);
+            Console.WriteLine("Конец 2 задачи");
+            #endregion
+
+            #region // контрольная работа вариант 10 №3
+            double y = 0, pi=3.14;
+            for (double xe = -2 * pi; xe <= 0; xe = xe + (pi / 5))
+            {
+                y = 2 * Math.Cos(xe + 2);
+
+                Console.WriteLine("xe={0:F3} | y={1:F3}", xe, y);
+            }
+            Console.WriteLine("Конец 3 задачи");
+            #endregion
+
+            #region // контрольная работа вариант 10 №4
+            double p = 1, n=2;
+            while (p<=100000)
+            {
+                p = p * n;
+                n = n + 2.5;
+            }
+            Console.WriteLine(n);
+            Console.WriteLine("Конец 4 задачи");
+            #endregion
+
+            #region // контрольная работа вариант 10 №5
+            double su=0,ne=4;
+            for (int i = 2; i <= 12; i = i + 2)
+            {
+                su = su + ne;
+                ne = ne * 2;
+                su = su - ne;
+                ne = ne * 2;
+            }
+            Console.WriteLine(su);
+            Console.WriteLine("Конец 5 задачи");
+            #endregion
+        }
+    }
+}
