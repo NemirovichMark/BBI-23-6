a65a325f27e2c2261a1d4b68bc7b7fbd18f0057c
IvanovDmitr <146060514+IvanovDmitr@users.noreply.github.com>
2023-10-18 19:25:52 +0300

    3 уровень 5 задача защита

---
+++ 3 уровень 5 задача.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_уровень_5_задача
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Math.PI / 5;
+            double b = Math.PI;
+            double h = Math.PI / 25;
+            double sum = 0;
+            double y = 0;
+            double lastNumber = 0;
+            int sign = 1;
+
+            for (int i = 1; ; i++)
+            {
+                double x = a;
+                while (x <= b)
+                {
+                    sum += sign * Math.Cos(i * x) / (i * i);
+                    y = (x * x - Math.PI * Math.PI / 2) / 4;
+                    Console.WriteLine($"{sum} {y}");
+                    lastNumber = sign * Math.Cos(i * x) / (i * i);
+
+                    x += h;
+                }
+
+                if (Math.Abs(lastNumber) < 0.0001)
+                {
+                    break;
+                }
+
+                sign *= -1;
+            }
+        }
+    }
+}
