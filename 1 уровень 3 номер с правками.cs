386bb1aaca2067273a961a896f68eccdd9028694
Kosarynskaya Anna <146060437+KosarynskayaA@users.noreply.github.com>
2023-10-24 09:21:51 +0300

    Add files via upload

---
+++ 1 уровень 3 номер с правками.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+double x4 = 1.5;
+double sum4 = 0.0;
+double xPower = 1.0;
+for (int i = 1; i <= 9; i++)
+{
+    double term = Math.Cos(i * x4) / xPower;
+    sum4 += term;
+    xPower *= x4;
+}
+
+Console.WriteLine(sum4);
