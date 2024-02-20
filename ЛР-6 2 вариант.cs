b514bcdb6565611f3a105bd5e0b80c170be1de6a
IvanovDmitr <146060514+IvanovDmitr@users.noreply.github.com>
2024-02-20 23:35:44 +0300

    Add files via upload

---
+++ ЛР-6 2 вариант.cs
@@ -0,0 +1,237 @@
+﻿using System;
+static class Hello
+{
+    //задание 1.1
+    //public struct Sportsmen
+    //{
+    //    string famile;
+    //    string name;
+    //    double rez1, rez2, rez;
+    //    public Sportsmen(string famile1, string name1, double rezz1, double rezz2)
+    //    {
+    //        famile = famile1;
+    //        name = name1;
+    //        rez1 = rezz1;
+    //        rez2 = rezz2;
+    //        rez = rez1 + rez2;
+    //    }
+    //    public double GetRez()
+    //    {
+    //        return rez;
+    //    }
+    //    public void Print()
+    //    {
+    //        Console.WriteLine($"{famile} {name} {rez}");
+    //    }
+    //}
+    //static Sportsmen[] Sort(Sportsmen[] sport)
+    //{
+    //    for (int i = 0; i < sport.Length; i++)
+    //    {
+    //        for (int j = i; j < sport.Length; j++)
+    //        {
+    //            if (sport[i].GetRez() < sport[j].GetRez())
+    //            {
+    //                Sportsmen student = sport[j];
+    //                sport[j] = sport[i];
+    //                sport[i] = student;
+    //            }
+    //        }
+    //    }
+    //    return sport;
+    //}
+
+    //public static void Main()
+    //{
+    //    Sportsmen[] sp = new Sportsmen[5];
+    //    sp[0] = new Sportsmen("Иванов", "Дмитрий", 3.15, 3.7);
+    //    sp[1] = new Sportsmen("Дорогой", "Александр", 4.12, 4.20);
+    //    sp[2] = new Sportsmen("Бруталов", "Фофан", 2.98, 3.34);
+    //    sp[3] = new Sportsmen("Палец", "Безымянный", 3.19, 3.11);
+    //    sp[4] = new Sportsmen("Просто", "Спортсмен", 5.11, 6.00);
+    //    for (int i = 0; i < sp.Length; i++)
+    //    {
+    //        sp[i].Print();
+    //    }
+    //    sp = Sort(sp);
+    //    Console.WriteLine();
+    //    for (int i = 0; i < sp.Length; i++)
+    //    {
+    //        sp[i].Print();
+    //    }
+    //}
+
+    //задание 2.2
+    //public struct Students
+    //{
+    //    string famile;
+    //    string name;
+    //    double rez1, rez2, rez3, rez;
+    //    public double GetRez1()
+    //    {
+    //        return rez1;
+    //    }
+    //    public double GetRez2()
+    //    {
+    //        return rez2;
+    //    }
+    //    public double GetRez3()
+    //    {
+    //        return rez3;
+    //    }
+    //    public double GetRez()
+    //    {
+    //        return rez;
+    //    }
+    //    public Students(string famile1, string name1, double rezz1, double rezz2, double rezz3)
+    //    {
+    //        famile = famile1; name = name1; rez1 = rezz1; rez2 = rezz2; rez3 = rezz3;
+    //        rez = (rezz1 + rezz2 + rezz3) / 3;
+    //    }
+    //    public void Print()
+    //    {
+    //        Console.WriteLine($"{famile} {name} {rez}");
+    //    }
+    //}
+    //static Students[] Sort(Students[] students)
+    //{
+    //    for (int i = 0; i < students.Length; i++)
+    //    {
+    //        for (int j = i; j < students.Length; j++)
+    //        {
+    //            if (students[i].GetRez() < students[j].GetRez())
+    //            {
+    //                Students student = students[j];
+    //                students[j] = students[i];
+    //                students[i] = student;
+    //            }
+    //        }
+    //    }
+    //    return students;
+    //}
+
+    //public static void Main()
+    //{
+    //    Students[] st = new Students[5];
+    //    st[0] = new Students("Иванов", "Дмитрий", 3, 3, 4);
+    //    st[1] = new Students("Дорогой", "Александр", 4, 4, 5);
+    //    st[2] = new Students("Бруталов", "Фофан", 2, 3, 4);
+    //    st[3] = new Students("Палец", "Безымянный", 3, 3, 3);
+    //    st[4] = new Students("Просто", "Спортсмен", 5, 2, 4);
+    //    st = Sort(st);
+    //    for (int i = 0; i < st.Length; i++)
+    //    {
+    //        st[i].Print();
+    //    }
+    //    Console.WriteLine();
+    //    for (int i = 0; i < st.Length; i++)
+    //    {
+    //        if (st[i].GetRez1() <= 2 || st[i].GetRez2() <= 2 || st[i].GetRez3() <= 2)
+    //        {
+
+    //        }
+    //        else
+    //        {
+    //            st[i].Print();
+    //        }
+    //    }
+    //}
+
+    //задание 3.4
+    //    public class Program
+    //{
+    //    struct Participant
+    //    {
+    //        string name;
+    //        int result;
+    //        public int Result()
+    //            {
+    //                return result;
+    //            }
+    //        public Participant(string name, int res)
+    //        {
+    //            this.name = name;
+    //            result = res;
+    //        }
+    //        public void InfoP()
+    //        {
+    //            Console.WriteLine($"имя: {name} результат: {result} ");
+    //        }
+    //    }
+    //    static Participant[] Sort(Participant[] list)
+    //    {
+    //        for (int i = 0; i < list.Length; i++)
+    //        {
+    //            for (int j = i; j < list.Length; j++)
+    //                if (list[i].Result() < list[j].Result())
+    //                {
+    //                    Participant person_now = list[j]; // меняем игроков 
+    //                    list[j] = list[i];
+    //                    list[i] = person_now;
+    //                }
+    //        }
+    //        return list;
+    //    }
+    //    public static void Main(string[] args)
+    //    {
+    //        int n1 = int.Parse(Console.ReadLine());
+    //        int n2 = int.Parse(Console.ReadLine());
+    //        Participant[] group1 = new Participant[n1];
+    //        Participant[] group2 = new Participant[n2];
+    //        Participant[] allgroup = new Participant[n2 + n1];
+
+    //        for (int i = 0; i < n1; i++)
+    //        {
+    //            string name_ = Console.ReadLine();
+    //            int result_ = int.Parse(Console.ReadLine());
+
+    //            Participant Person = new Participant(name_, result_);
+
+    //            group1[i] = Person;
+    //            allgroup[i] = Person;
+    //        }
+
+    //        for (int i = 0; i < n2; i++)
+    //        {
+    //            string name_ = Console.ReadLine();
+    //            int result_ = int.Parse(Console.ReadLine());
+
+    //            Participant Person = new Participant(name_, result_);
+
+    //            group2[i] = Person;
+    //            allgroup[i + n1] = Person;
+    //        }
+
+    //        group1 = Sort(group1);
+    //        group2 = Sort(group2);
+    //        allgroup = Sort(allgroup);
+
+    //        Console.WriteLine("1 группа:");
+    //        for (int i = 0; i < n1; i++)
+    //        {
+    //            Console.Write($"{i + 1} место:"); ;
+    //            group1[i].InfoP();
+    //            Console.WriteLine();
+    //        }
+    //        Console.WriteLine();
+
+    //        Console.WriteLine("2 группа:");
+    //        for (int i = 0; i < n2; i++)
+    //        {
+    //            Console.Write($"{i + 1} место:"); ;
+    //            group2[i].InfoP();
+    //            Console.WriteLine();
+    //        }
+    //        Console.WriteLine();
+
+    //        Console.WriteLine("Все участники:");
+    //        for (int i = 0; i < n1 + n2; i++)
+    //        {
+    //            Console.Write($"{i + 1} место:"); ;
+    //            allgroup[i].InfoP();
+    //            Console.WriteLine();
+    //        }
+    //        Console.WriteLine();
+    //    }
+    //}
+}
