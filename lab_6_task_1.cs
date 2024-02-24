399a6bad0ca04b872961bac62965c504fabac2f3
marials_s <79412013+Masha-als@users.noreply.github.com>
2024-02-24 14:56:35 +0300

    Add files via upload

---
+++ lab_6_task_1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+/*
+1) Составить программу для обработки результатов кросса на 500 м для женщин.  
+Для каждой участницы ввести фамилию, группу, фамилию преподавателя,
+результат. Получить результирующую таблицу, упорядоченную по результатам, в  
+которой содержится также информация о выполнении норматива. Определить  
+суммарное количество участниц, выполнивших норматив.
+*/
+
+
+namespace lab_6
+{
+    class Program
+    {
+        public struct Participant // объявление структуры
+        {
+
+            private string surname;
+            private int group;
+            private string surnameTeacher;
+            private int result;
+            private int norma;
+            private bool pass;
+
+
+            public Participant(string ssurname, int ggroup, string ssurnameTeacher, int rresult) //объявление конструктора
+            {
+                surname = ssurname;
+                group = ggroup;
+                surnameTeacher = ssurnameTeacher;
+                result = rresult;
+                norma = 90;
+                pass = false;
+                if (result <= norma) { pass = true; }
+            }
+
+            public int Result { get { return result; } }
+            public bool Passed { get { return pass; } }
+            public void Print(string text = "Некорректная информация", string add = "Норматив не выполнен")
+            {
+                if (pass) { add = "Норматив выполнен"; }
+                if (surname != null && surnameTeacher != null && group > 0 && result > 0)
+                { text = surname + " " + group + " " + surnameTeacher + " " + result + " - " + add; }
+                Console.WriteLine(text);
+            }
+
+
+            static void Main(string[] args)
+            {
+                Console.Write("Введите количество участников кросса: ");
+                int k = int.Parse(Console.ReadLine());
+                Participant[] participants = new Participant[k];
+                string surname, surnameTeacher;
+                int group, result;
+                int count = 0;
+
+                for (int i = 0; i < k; i++)
+                {
+                    Console.WriteLine("Введите следующие данные: фамилия, группа, фамилия преподавателя, результат кросса, отдельно на каждой строке:");
+                    surname = Console.ReadLine();
+                    group = int.Parse(Console.ReadLine());
+                    surnameTeacher = Console.ReadLine();
+                    result = int.Parse(Console.ReadLine());
+                    participants[i] = new Participant(surname, group, surnameTeacher, result);
+                    if (participants[i].Passed == true)
+                    {
+                        count++;
+                    }
+                }
+
+                // gnomesort массива 
+                Participant d;
+                for (int i = 1; i < k; i++)
+                {
+                    for (int j = i; j > 0 && participants[j].Result < participants[j - 1].Result; j--)
+                    {
+                        d = participants[j];
+                        participants[j] = participants[j - 1];
+                        participants[j - 1] = d;
+                    }
+                }
+                Console.WriteLine();
+                for (int i = 0; i < k; i++)
+                {
+                    participants[i].Print();
+                }
+                Console.WriteLine("Выполнило норматив: " + count);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+
+/*
+
+namespace Lessons
+{
+    class Program
+    {
+      
+        // метод/функция 
+        static int Foo(int value)
+        {
+            value = 5;
+            return value;
+        }
+        static void Main(string[] args)
+        {
+            int a = 1;
+            a = Foo(a);
+            Console.WriteLine(a);
+        }
+
+
+
+    }
+    
+}
+
+*/
---
+++ lab_6_task_3.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+/*
+3)Соревнования по футболу между командами проводятся в два этапа. Для  
+проведения первого этапа участники разбиваются на две группы по 12 команд.  
+Для проведения второго этапа выбирается 6 лучших команд каждой группы по  
+результатам первого этапа. Составить список команд участников второго этапа.
+*/
+
+
+namespace lab_6_task_3
+{
+    class Match
+    {
+        public struct Team // структура
+        {
+
+            public Team() // конструктор, я не до конца понимаю для чего он используется, поэтому не использую в этом коде
+            {
+
+            }
+
+            static string[] Winners(string[] team, int[] count) 
+            {
+                string[] winners = new string[6];
+                int j = 0;
+                int i = 0;
+                int[] countt = count;
+                while (j < 6)
+                {
+                    if (i < 11) { i++; }
+                    else { i = 0; }
+                    if (countt[i] == countt.Max()) { countt[i] = 0; winners[j] = team[i]; j++; }
+                    if (j == 6) { break; }
+                    
+                }
+                return winners;
+            }
+
+            /* для самопроверки
+            static void Print_int(int[] team)
+            {
+                for (int i = 0; i < team.Length; i++) { Console.Write(team[i] + " "); }
+                Console.WriteLine();
+            }
+            */
+            static void Print(string[] team)
+            {
+                for (int i = 0; i < team.Length; i++) { Console.Write(team[i] + " "); }
+                Console.WriteLine();
+            }
+
+            static void Main(string[] args)
+            {
+                string[] teamA = "A, B, C, D, E, F, G, H, I, J, K, L".Split(", ");
+                string[] teamB = "M, N, O, P, Q, R, S, T, U, V, W, X".Split(", ");
+                int[] countA = new int[teamA.Length];
+                int[] countB = new int[teamB.Length];
+                Random rand = new Random();
+                for (int i = 0; i < 12; i++) // команда ABC
+                {
+                    for (int j = 0; j < 12; j++) // команда MNO
+                    {
+                        if (rand.Next(0, 10) > rand.Next(0, 10)) { countA[i] += 1; }
+                        if (rand.Next(0, 10) < rand.Next(0, 10)) { countB[j] += 1; }
+                    }
+                }
+                /* самопроверка: 
+                Console.Write("Количество побед 1 команды: ");
+                Print_int(countA);
+                Console.Write(":Количество побед 2 команды: ");
+                Print_int(countB);
+                */
+
+                Console.WriteLine("Состав 1 команды: ");
+                Print(Winners(teamA, countA));
+                Console.WriteLine("Состав 2 команды: ");
+                Print(Winners(teamB, countB));
+            }
+
+        }
+    }
+}
