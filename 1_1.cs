1a3bd1d6297d97393cb4b0cc3f1eba2db0c3940a
ArdashirovaAmina <146060724+ArdashirovaAmina@users.noreply.github.com>
2024-03-02 13:48:06 +0500

    Add files via upload
    Первая лабораторная 1,2,3 уровни 1 задачи

---
+++ 1_1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        //Заполнение массива людей
+        Jumper[] Jumpers = new Jumper[]
+        {
+                new Jumper("Шолохов", "ENF"),
+                new Jumper("Максютов", "LNM"),
+                new Jumper("Леонов", "DMT"),
+                new Jumper("Леонтьев", "ГРМ"),
+                new Jumper("Вахрушев", "ДДТ"),
+        };
+
+        //Заполнение длины прыжков
+        for (int i = 0; i < Jumpers.Length; i++)
+        {
+            Jumpers[i].SetJumps();
+        }
+
+        //Сортировка по месту
+        BubbleSort(Jumpers);
+        Console.WriteLine($"Фамилия - Общество - Прыжок 1 - Прыжок 2");
+        for (int i = Jumpers.Length - 1; i >= 0; i--)
+        {
+            Jumpers[i].Print();
+        }
+        Console.ReadKey();
+    }
+
+    static void Swap(ref Jumper e1, ref Jumper e2)
+    {
+        var temp = e1;
+        e1 = e2;
+        e2 = temp;
+    }
+
+    //сортировка пузырьком
+    static void BubbleSort(Jumper[] array)
+    {
+        var len = array.Length;
+        for (var i = 1; i < len; i++)
+        {
+            for (var j = 0; j < len - i; j++)
+            {
+                if (array[j].GetSumJumps() > array[j + 1].GetSumJumps())
+                {
+                    Swap(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+    }
+
+    struct Jumper
+    {
+        private string SecondName;
+        private string Company;
+
+        private float[] JumpTrys;
+
+        public Jumper(string secondName, string company)
+        {
+            SecondName = secondName;
+            Company = company;
+            JumpTrys = new float[2] { 0f, 0f };
+        }
+
+        public void SetJumps()
+        {
+            for (int i = 0; i < JumpTrys.Length; i++)
+            {
+                Console.Write($"Введите длину прыжка {i + 1} - Участника {SecondName}: ");
+                float jumpValue = float.Parse(Console.ReadLine());
+
+                JumpTrys[i] = jumpValue;
+            }
+            Console.WriteLine();
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{SecondName} - {Company} - {JumpTrys[0]} - {JumpTrys[1]} - ({GetSumJumps()})");
+        }
+
+        public float GetSumJumps() => JumpTrys[0] + JumpTrys[1];
+    }
+}
---
+++ 2_1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        //Заполнение массива людей
+        Student[] Students = new Student[]
+        {
+                new Student("Шолохов А.В", new double[] {2, 2, 4, 5}),
+                new Student("Горохов М.И", new double[] {4, 4, 3, 5}),
+                new Student("Котова В.И", new double[] {5, 4, 5, 5}),
+                new Student("Дегтярев З.В", new double[] {3, 4, 5, 5}),
+                new Student("Тихонов Д.Е", new double[] {4, 2, 3, 4}),
+        };
+
+        //Сортировка по среднему баллу
+        BubbleSort(Students);
+        Console.WriteLine($"ФИО - Средний Балл");
+        for (int i = Students.Length - 1; i >= 0; i--)
+        {
+            if (Students[i].GetMiddleMark() >= 4)
+            {
+                Students[i].Print();
+            }
+        }
+        Console.ReadKey();
+    }
+
+    static void Swap(ref Student e1, ref Student e2)
+    {
+        var temp = e1;
+        e1 = e2;
+        e2 = temp;
+    }
+
+    //сортировка пузырьком
+    static void BubbleSort(Student[] array)
+    {
+        var len = array.Length;
+        for (var i = 1; i < len; i++)
+        {
+            for (var j = 0; j < len - i; j++)
+            {
+                if (array[j].GetMiddleMark() > array[j + 1].GetMiddleMark())
+                {
+                    Swap(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+    }
+
+    struct Student
+    {
+        public string FIO { get; private set; }
+
+        private double[] Marks;
+        public Student(string fio, double[] marks)
+        {
+            FIO = fio;
+            Marks = marks;
+        }
+
+        public double GetMiddleMark()
+        {
+            double sum = 0;
+            for (int i = 0; i < Marks.Length; i++)
+            {
+                sum += Marks[i];
+            }
+
+            return sum / Marks.Length;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{FIO} - {GetMiddleMark()}");
+        }
+    }
+}
